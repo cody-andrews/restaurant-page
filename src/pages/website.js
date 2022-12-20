395b8da17a8816f7fe0ba7bd3cdefde0f44f6b41
Cody Andrews <cera117@gmail.com>
2022-12-20 15:05:32 -0800

    added function to toggle active button

--- src/pages/website.js
+++ src/pages/website.js
@@ -50,6 +50,19 @@ function createNav(){
 
 }
 
+function setActiveButton(button)    {
+    const buttons = document.querySelectorAll('.button-nav');
+
+    buttons.forEach((button)=>    {
+        if (button !== this)    {
+            button.classList.remove("active");
+            }
+        }) ;
+
+        button.classList.add('active')
+    }
+
+
 function initWebsite() {
 
     const content = document.getElementById('content');
