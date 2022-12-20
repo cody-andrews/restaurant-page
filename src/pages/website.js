d148bd421b462f7697d5eeebba51d2afcfd7fe07
Cody Andrews <cera117@gmail.com>
2022-12-20 13:33:30 -0800

    added nav bar and updated styling

--- src/pages/website.js
+++ src/pages/website.js
@@ -14,6 +14,42 @@ function createHeader(){
     return header;
 }
 
+function createNav(){
+    const nav = document.createElement('nav');
+
+    const homeButton = document.createElement('button');
+    homeButton.classList.add('button-nav');
+    homeButton.textContent = 'Home';
+    homeButton.addEventListener('click', (e) => {
+        if (e.target.classList.contains('active')) return;
+        setActiveButton(homeButton);
+        loadHome();
+    })
+    const menuButton = document.createElement('button');
+    menuButton.classList.add('button-nav');
+    menuButton.textContent = 'Menu';
+    menuButton.addEventListener('click', (e) => {
+        if (e.target.classList.contains('active')) return;
+        setActiveButton(menuButton);
+        loadMenu();
+    })
+    const contactButton = document.createElement('button');
+    contactButton.classList.add('button-nav');
+    contactButton.textContent = 'Contact';
+    contactButton.addEventListener('click', (e) => {
+        if (e.target.classList.contains('active')) return;
+        setActiveButton(contactButton);
+        loadContact();
+    })
+
+    nav.appendChild(homeButton);
+    nav.appendChild(menuButton);
+    nav.appendChild(contactButton);
+
+    return nav;
+
+}
+
 function initWebsite() {
 
     const content = document.getElementById('content');
